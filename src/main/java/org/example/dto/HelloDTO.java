ec5aeb70a8681e89e8b4a0cc0a9b982e09c279d3
Ragunathan <ragubaskarr@gmail.com>
2023-05-20 19:46:47 +0530

    Initial Commit

---
+++ src/main/java/org/example/dto/HelloDTO.java
@@ -0,0 +1,18 @@
+package org.example.dto;
+
+public class HelloDTO {
+
+    private String message;
+
+    @SuppressWarnings("unused")
+    public HelloDTO() {
+    }
+
+    public HelloDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
